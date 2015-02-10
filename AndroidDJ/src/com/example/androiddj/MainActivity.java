7394e9a351459e3b2fbd56ce9473b1c680af68e8
Vipin <vipina82@gmail.com>
2015-02-10 20:01:25 +0530

    UI Updated

--- AndroidDJ/src/com/example/androiddj/MainActivity.java
+++ AndroidDJ/src/com/example/androiddj/MainActivity.java
@@ -1,96 +1,60 @@
 package com.example.androiddj;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import java.util.ArrayList;
-import java.util.List;
-import android.app.Activity;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
-	private String tag = "DJ Debugging";
-	ListView list;
-    private List<String> List_file;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		Log.i(tag,"Going to call oncreate");
-		super.onCreate(savedInstanceState);
-		Log.i(tag,"calling setcontent view");
-		setContentView(R.layout.activity_main);
-		Log.i(tag,"Going to create list_file");
-		List_file = new ArrayList<String>();
-		Log.i(tag,"Going to create list");
-        list = (ListView)findViewById(R.id.listview);
-		Log.i(tag,"Going to call create list view");
-        CreateListView();
-		Log.i(tag,"Finished create list view");
-	}
+public class MainActivity extends Activity
+{
+    private String tag = "DJ Debugging";
+    private Button hostButton;
+    private Button joinButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i(tag,"Going to call oncreate");
+        super.onCreate(savedInstanceState);
+        Log.i(tag,"calling setcontent view");
+        setContentView(R.layout.activity_main);
+        Log.i(tag,"View created");
+        hostButton = (Button) findViewById(R.id.host_button);
+        joinButton = (Button) findViewById(R.id.join_button);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-	
-	/*
-	 * This function will create the list view, put this function in files whereever you require
-	 * any scroll list
-	*/
-	private void CreateListView()
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onHostClicked(View button)
     {
-		for(int i=0;i<10;i++)
-		{
-			List_file.add("Song"+Integer.toString(i));
-		}
-		final int indexChanged[] = new int[1];
-		final String initialValue[] = new String[1];
-		indexChanged[0] = -1;
-		initialValue[0] = "";
-         list.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,List_file));
-         list.setOnItemClickListener(new OnItemClickListener()
-           {
-                @Override
-                public void onItemClick(AdapterView<?> parent, final View view, final int position,long id)
-                {
-                    //args2 is the listViews Selected index
-                	if(indexChanged[0] != -1)
-                	{
-                		List_file.set(indexChanged[0], initialValue[0]);
-                	}
-                	indexChanged[0] = position;
-                	initialValue[0] = List_file.get(position);
-                	//List_file.set(position, List_file.get(position) + " is selected");
-                	view.animate().setDuration(200).alpha(0).withEndAction(new Runnable() {
-                      @Override
-                      public void run() {
-                        List_file.set(position, List_file.get(position) + " is selected");
-                        ArrayAdapter adapter = (ArrayAdapter) list.getAdapter();
-                        adapter.notifyDataSetChanged();
-                        view.setAlpha(1);
-                      }
-                    });
-                }
-           });
+        Log.i(tag,"Host is clicked");
+        Intent hostActivity = new Intent(this,StartView.class);
+        startActivity(hostActivity);
+    }
+
+    public void onJoinClicked(View button)
+    {
+        Log.i(tag,"Join is clicked");
+        Intent joinActivity = new Intent(this,StartView.class);
+        startActivity(joinActivity);
     }
 }
--- AndroidDJ/src/com/example/androiddj/StartView.java
+++ AndroidDJ/src/com/example/androiddj/StartView.java
@@ -1,68 +1,107 @@
 package com.example.androiddj;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.Activity;
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.AdapterView;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.AdapterView.OnItemClickListener;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
-public class StartView extends Activity
-{
-	private String tag = "DJ Debugging";
-	private Button hostButton;
-	private Button joinButton;
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		Log.i(tag,"Going to call oncreate");
-		super.onCreate(savedInstanceState);
-		Log.i(tag,"calling setcontent view");
-		setContentView(R.layout.start_view);
-		Log.i(tag,"View created");
-		hostButton = (Button) findViewById(R.id.host_button);
-		joinButton = (Button) findViewById(R.id.join_button);
-	}
+import java.util.ArrayList;
+import java.util.List;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+public class StartView extends Activity {
+    private String tag = "DJ Debugging";
+    LinearLayout list;
+    private List<RelativeLayout> List_file;
+    private ArrayAdapter<RelativeLayout> listAdapter;
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-	
-	public void onHostClicked(View button)
-	{
-		Log.i(tag,"Host is clicked");
-		Intent i = new Intent(this,MainActivity.class);
-		startActivity(i);
-	}
-	
-	public void onJoinClicked(View button)
-	{
-		Log.i(tag,"Join is clicked");
-		Intent i = new Intent(this,MainActivity.class);
-		startActivity(i);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i(tag,"Going to call oncreate");
+        super.onCreate(savedInstanceState);
+        Log.i(tag, "calling setcontent view");
+        setContentView(R.layout.start_view);
+        Log.i(tag, "Going to create list_file");
+        List_file = new ArrayList<RelativeLayout>();
+        Log.i(tag,"Going to create list");
+        list = (LinearLayout)findViewById(R.id.listview);
+
+        Log.i(tag,"Going to call create list view");
+        CreateListView();
+        Log.i(tag,"Finished create list view");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /*
+     * This function will create the list view, put this function in files whereever you require
+     * any scroll list
+    */
+    private void CreateListView()
+    {
+        for(int i=0;i<15;i++)
+        {
+            RelativeLayout rlt = new CustomLayout(getApplicationContext(),"Song "+Integer.toString(i));
+            View ruler = new View(rlt.getContext());
+            ruler.setBackgroundColor(Color.LTGRAY);
+            list.addView(rlt);
+            list.addView(ruler, new ViewGroup.LayoutParams( ViewGroup.LayoutParams.MATCH_PARENT, 2));
+        }
+        View ruler = new View(getApplicationContext());
+        ruler.setBackgroundColor(Color.LTGRAY);
+        list.addView(ruler, new ViewGroup.LayoutParams( ViewGroup.LayoutParams.MATCH_PARENT, 2));
+
+//        listAdapter = new ArrayAdapter<RelativeLayout>(StartView.this, R.layout.list_item_layout,List_file);
+//        list.setAdapter(listAdapter);
+
+//        list.setOnItemClickListener(new OnItemClickListener()
+//        {
+//            int indexChanged=-1;
+//            String initialValue="";
+//
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, final View view, final int position,long id)
+//            {
+//                TextView song;
+//                CustomLayout rlt;
+//                //args2 is the listViews Selected index
+//                if(indexChanged != -1)
+//                {
+//                    rlt = (CustomLayout)List_file.get(indexChanged);
+//                    rlt.song.setText(initialValue);
+//                }
+//
+//                indexChanged = position;
+//                rlt = (CustomLayout)List_file.get(indexChanged);
+//                initialValue = (String)rlt.song.getText();
+//                rlt.song.setText(initialValue+" is selected");
+//
+////                listAdapter.notifyDataSetChanged();
+//            }
+//        });
+
+    }
 }
