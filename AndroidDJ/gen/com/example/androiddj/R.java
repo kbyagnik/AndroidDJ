8b6bfe7a66c271043f0fb2e1d3a4a595d2d77eea
Ankit <ankitkhokhar@iitrpr.ac.in>
2015-03-01 19:02:09 +0530

    upvote downvote button added

--- AndroidDJ/gen/com/example/androiddj/R.java
+++ AndroidDJ/gen/com/example/androiddj/R.java
@@ -10,6 +10,10 @@ package com.example.androiddj;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int GREEN=0x7f040001;
+        public static final int RED=0x7f040000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -18,32 +22,47 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int host_button=0x7f080001;
-        public static final int join_button=0x7f080002;
-        public static final int listview=0x7f080000;
+        public static final int action_settings=0x7f09000a;
+        public static final int closed_party_button=0x7f090002;
+        public static final int downvote=0x7f090006;
+        public static final int host_button=0x7f090008;
+        public static final int invitation_party_button=0x7f090003;
+        public static final int join_button=0x7f090009;
+        public static final int listViewItem=0x7f090004;
+        public static final int listview=0x7f090000;
+        public static final int publicparty=0x7f090001;
+        public static final int upvote=0x7f090005;
+        public static final int votedByUser=0x7f090007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int start_view=0x7f030001;
+        public static final int host_options=0x7f030001;
+        public static final int listview_content=0x7f030002;
+        public static final int start_view=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int footer_layout=0x7f050003;
-        public static final int hello_world=0x7f050001;
-        public static final int host_button=0x7f050004;
-        public static final int join_button=0x7f050005;
+        public static final int VotingTextView=0x7f060006;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int closed_party=0x7f06000a;
+        public static final int downvote=0x7f060008;
+        public static final int footer_layout=0x7f060003;
+        public static final int hello_world=0x7f060001;
+        public static final int host_button=0x7f060004;
+        public static final int invitation_party=0x7f06000b;
+        public static final int join_button=0x7f060005;
+        public static final int open_party=0x7f060009;
+        public static final int upvote=0x7f060007;
     }
     public static final class style {
         /** 
@@ -67,10 +86,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- AndroidDJ/src/com/example/androiddj/MainActivity.java
+++ AndroidDJ/src/com/example/androiddj/MainActivity.java
@@ -18,7 +18,9 @@ import android.widget.AdapterView.OnItemClickListener;
 public class MainActivity extends Activity {
 	private String tag = "DJ Debugging";
 	ListView list;
-    private List<String> List_file;
+	ListViewAdapter adapter;
+	final int pos = -1;
+    private ArrayList<String> songs;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		Log.i(tag,"Going to call oncreate");
@@ -26,11 +28,24 @@ public class MainActivity extends Activity {
 		Log.i(tag,"calling setcontent view");
 		setContentView(R.layout.activity_main);
 		Log.i(tag,"Going to create list_file");
-		List_file = new ArrayList<String>();
+		songs = addSongs();
 		Log.i(tag,"Going to create list");
         list = (ListView)findViewById(R.id.listview);
+        adapter = new ListViewAdapter(songs,MainActivity.this,pos);
+        list.setAdapter(adapter);
+        Log.i(tag,"Adapter set");
+        Log.i(tag,"Defining on click listener");
+        list.setOnItemClickListener(new OnItemClickListener() {
+        	@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+        		Log.i(tag,"Item at " + Integer.toString(position) + " is clicked");
+				adapter.setPosition(position);
+				position = pos;
+				adapter.notifyDataSetChanged();
+			}
+		});
 		Log.i(tag,"Going to call create list view");
-        CreateListView();
 		Log.i(tag,"Finished create list view");
 	}
 
@@ -53,21 +68,58 @@ public class MainActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 	
+	private ArrayList<String> addSongs()
+	{
+		ArrayList<String> songs = new ArrayList<String>();
+		for(int i=0;i<10;i++)
+		{
+			songs.add("Song "+Integer.toString(i+1));
+		}
+		return songs;
+	}
+	
 	/*
 	 * This function will create the list view, put this function in files whereever you require
 	 * any scroll list
 	*/
-	private void CreateListView()
+	/*
+	 package com.example.androiddj;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
+public class ListViewAdapter
+{
+	final List<String> songs;
+	final ListView list;
+	final Context context;
+	
+	public ListViewAdapter(ArrayList<String> songs,ListView list,Context context)
+	{		
+        this.songs = songs;
+        this.list = list;
+        this.context = context;
+		CreateListView();
+	}
+	
+	public void CreateListView()
     {
 		for(int i=0;i<10;i++)
 		{
-			List_file.add("Song"+Integer.toString(i));
+			((List<String>) ((View) songs).getParent()).add("Song"+Integer.toString(i));
 		}
 		final int indexChanged[] = new int[1];
 		final String initialValue[] = new String[1];
 		indexChanged[0] = -1;
 		initialValue[0] = "";
-         list.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1,List_file));
+         list.setAdapter(new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1,songs));
          list.setOnItemClickListener(new OnItemClickListener()
            {
                 @Override
@@ -76,15 +128,15 @@ public class MainActivity extends Activity {
                     //args2 is the listViews Selected index
                 	if(indexChanged[0] != -1)
                 	{
-                		List_file.set(indexChanged[0], initialValue[0]);
+                		songs.set(indexChanged[0], initialValue[0]);
                 	}
                 	indexChanged[0] = position;
-                	initialValue[0] = List_file.get(position);
-                	//List_file.set(position, List_file.get(position) + " is selected");
+                	initialValue[0] = songs.get(position);
+                	//songs.set(position, songs.get(position) + " is selected");
                 	view.animate().setDuration(200).alpha(0).withEndAction(new Runnable() {
                       @Override
                       public void run() {
-                        List_file.set(position, List_file.get(position) + " is selected");
+                        songs.set(position, songs.get(position) + " is selected");
                         ArrayAdapter adapter = (ArrayAdapter) list.getAdapter();
                         adapter.notifyDataSetChanged();
                         view.setAlpha(1);
@@ -93,4 +145,10 @@ public class MainActivity extends Activity {
                 }
            });
     }
+	
+}
+
+	 */
+	
+	
 }
